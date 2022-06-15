2229fc0f4fd3976ec2e2b354769fa8756f799abb
AugustoMagalhaes <augustommg@gmail.com>
2022-06-15 16:21:46 -0300

    chore: adiciona evento ao botao e condicional para valores 'undefined'.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const boxSection = document.querySelector('.box');
 const inputColuna = document.querySelector('#colunas');
 const inputLinha = document.querySelector('#linhas');
+const sendBtn = document.querySelector('.btn');
 
 function generateBox() {
-  const colunaValue = Number(inputColuna.value);
-  const linhaValue = Number(inputLinha.value);
+  const colunaValue = Number(inputColuna.value) || 1;
+  const linhaValue = Number(inputLinha.value) || 1;
   boxSection.innerHTML = '';
 
   for (let row = 0; row < linhaValue; row += 1) {
@@ -18,4 +19,9 @@ function generateBox() {
     boxSection.appendChild(newRow)
   }
   console.log(typeof colunaValue, colunaValue)
+}
+
+window.onload = function() {
+  sendBtn.addEventListener('click', generateBox);
+  generateBox();
 }
